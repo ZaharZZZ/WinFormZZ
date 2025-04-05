46633aece52d445dcda1339fd70e741d25d87dab
st56 <zahark456@gmail.com>
2025-04-05 08:38:57 +0300

    Сделали небольшие тесты для треугольника

---
+++ Testing/TTriangle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyLib;
+
+namespace Testing
+{
+    [TestClass]
+    public class TTriangle
+    {
+        [TestMethod]
+        public void CalculatePerimeter_ValidTriangle_ReturnsCorrectPerimeter()
+        {
+            // Arrange
+            Triangle triangle = new Triangle(3, 4, 5);
+            double expectedPerimeter = 12;
+
+            // Act
+            double actualPerimeter = triangle.CalculatePerimeter();
+
+            // Assert
+            Assert.AreEqual(expectedPerimeter, actualPerimeter);
+        }
+
+        [TestMethod]
+        public void CalculateArea_ValidTriangle_ReturnsCorrectArea()
+        {
+            // Arrange
+            Triangle triangle = new Triangle(3, 4, 5);
+            double expectedArea = 6;
+
+            // Act
+            double actualArea = triangle.CalculateArea();
+
+            // Assert
+            Assert.AreEqual(expectedArea, actualArea, 0.001);
+        }
+        [TestMethod]
+        
+        public void Triangle_ZeroSide_ThrowsArgumentException()
+        {
+            new Triangle(0, 4, 5); 
+        }
+    }
+}
