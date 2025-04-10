14136d96a16e84e43ed1cd5dbe9459c11b3d897d
st50 <zahar@gamil.com>
2025-04-10 13:24:48 +0300

    Переделать тесты

--- Testing/TTriangle.cs
+++ Testing/TTriangle.cs
@@ -8,37 +8,12 @@ namespace Testing
     public class TTriangle
     {
         [TestMethod]
-        public void CalculatePerimeter_ValidTriangle_ReturnsCorrectPerimeter()
-        {
-            // Arrange
-            TriangleZ triangle = new TriangleZ(3, 4, 5);
-            double expectedPerimeter = 12;
-
-            // Act
-            double actualPerimeter = triangle.CalculatePerimeter();
-
-            // Assert
-            Assert.AreEqual(expectedPerimeter, actualPerimeter);
-        }
-
-        [TestMethod]
-        public void CalculateArea_ValidTriangle_ReturnsCorrectArea()
-        {
-            // Arrange
-            TriangleZ triangle = new TriangleZ(3, 4, 5);
-            double expectedArea = 6;
-
-            // Act
-            double actualArea = triangle.CalculateArea();
-
-            // Assert
-            Assert.AreEqual(expectedArea, actualArea, 0.001);
-        }
-        [TestMethod]
         
-        public void Triangle_ZeroSide_ThrowsArgumentException()
+        public void TestTrianglSExistence()
         {
-            new TriangleZ(0, 4, 5); 
+            
         }
+
+       
     }
 }
